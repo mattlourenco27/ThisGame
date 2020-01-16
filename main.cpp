4f2fd9a58d4e54b590601787e0495479afe42669
mattlourenco27 <matt.lourenco27@gmail.com>
2020-01-15 23:02:14 -0500

    Debugged Line and point drawing functionality

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 #define TESTMAP "../maps/testmap1.1.txt"
+#define TESTLOADMAP "../maps/testmap1.txt"
 
 int main() {
     cout << "Creating a few test maps..." << endl;
@@ -30,6 +31,36 @@ int main() {
     painter.drawRect(0,0,9,9,'H');
     painter.drawRect(10, 10, 20, 19, 'S', true);
     cout << (painter.save("../maps/testmap1.3.txt")? "Saved": "Save unsuccessful") << endl;
+
+    cout << "Testing width and height constructor and saving to testmap1.4 (10x10)" << endl;
+    mapPainter p0(10, 10);
+    cout << (p0.save("../maps/testmap1.4.txt")? "Saved": "Save unsuccessful") << endl;
+    cout << "Testing saving without defining destination" << endl;
+    cout << (p0.save()? "Saved": "Save unsuccessful") << endl;
+
+    cout << "Testing source file constructor, making an edit, and saving to testmap1.5" << endl;
+    mapPainter p1(TESTLOADMAP);
+    p1.setDest("../maps/testmap1.5.txt");
+    p1.drawRect(1, 1, p1.getWidth() - 2, 1, 'A');
+    cout << (p1.save()? "Saved": "Save unsuccessful") << endl;
+
+    cout << "Creating a new map with the same painter" << endl;
+    p1.newMap();
+    p1.setDest("../maps/testmap1.6.txt");
+    cout << (p1.save()? "Saved": "Save unsuccessful") << endl;
+
+    cout << "Loading previous map, drawing lines, and saving to testmap1.6" << endl;
+    p1.loadMap("../maps/testmap1.2.txt");
+    p1.drawLine(1,1,6,6,'A');
+    p1.drawLine(18,18,13,13, 'B');
+    p1.drawLine(38,1,33,6, 'C');
+    p1.drawLine(1,18,6,13, 'D');
+    cout << (p1.save()? "Saved": "Save unsuccessful") << endl;
+
+    cout << "Drawing points and saving to testmap1.7" << endl;
+    p1.drawPoint(1,1,'X');
+    p1.drawPoint(18,18, 'Y');
+    cout << (p1.save("../maps/testmap1.7.txt")? "Saved": "Save unsuccessful") << endl;
 
     return 0;
 }
